22968dc6ec4b0c701a26da0600a2d63524856425
DESKTOP-KD20L24\PhamTu <dendroidking02121@gmail.com>
2017-05-24 21:36:29 -0700

    change 24/5

--- session6(change)/sokoban(change).py
+++ session6(change)/sokoban(change).py
@@ -13,14 +13,8 @@ box2={
     "y": 4
     }
 boxes=[
-    {
-    "x": 1,
-    "y": 2
-    },
-    {
-    "x": 2,
-    "y": 4
-    }   
+    box, box2
+       
     ]
 
 
@@ -104,33 +98,37 @@ while True:
     ## xu ly dau vao
     
     dx, dy = input_process(direction)
-    if box["x"] == pusher_x and box["y"] == pusher_y:
+    if box["x"] == pusher_x + dx and box["y"] == pusher_y + dy:
+        print("vao1")
         if is_in_map(box["x"] + dx, box["y"] + dx, size_x, size_y):
+            print("vao1.1")
             if box["x"] + dx != box2["x"] and box["y"] != box2["y"]:
                 box["x"] += dx
                 box["y"] += dy
                 pusher_x += dx
-                pusher_y +=dy
+                pusher_y += dy
+                
             else:
-                print("you can push bro:")
+                print("You can push bro:")
         else:
             print("Pass")
-    elif box2["x"] == pusher_x and box2["y"] == pusher_y:
+    elif box2["x"] == pusher_x + dx and box2["y"] == pusher_y + dy:
+        print("vao2")
         if is_in_map(box2["x"] + dx, box2["y"] + dx, size_x, size_y):
-            if box["x"] + dx != box2["x"] and box["y"] != box2["y"]:
+            print("Vao2.1")
+            if box2["x"] + dx != box["x"] and box2["y"] != box["y"]:
                 box2["x"] += dx
                 box2["y"] += dy
                 pusher_x += dx
-                
                 pusher_y += dy
             else:
-                print("You can push")
+                print("You can not push")
         else:
             print("Waitting")
     else:
-        next_pusher_x=pusher_x+dx
-        next_pusher_y=pusher_y+dy
-        if(is_in_map(pusher_x+dx,next_pusher_y,size_x,size_y)):
+        next_pusher_x = pusher_x + dx
+        next_pusher_y = pusher_y + dy
+        if is_in_map(pusher_x+dx,next_pusher_y,size_x,size_y):
                 
             pusher_x += dx
             pusher_y += dy
